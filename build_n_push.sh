7bdb224dac085f18ac20fe609c185e156882c5d2
Mykhailo Kan <mykhailo.kan@giss.pl>
2024-06-13 14:34:42 +0200

    stuff

---
+++ build_n_push.sh
@@ -0,0 +1,1 @@
+docker build -t <yourusername>/jenkins-agent-dind -f ./jenkins_agent_dind.Dockerfile . 
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://medium.com/@yassine.essadraoui_78000/jenkins-docker-in-docker-b7630c7b9364
